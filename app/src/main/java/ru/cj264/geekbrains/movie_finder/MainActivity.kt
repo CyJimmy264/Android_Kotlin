9069afc1b83a435e50d35198f82d89e82c5f0791
mveynberg <mv@cj264.ru>
2021-04-30 18:11:10 +0500

    Step 4. Add button with click listener

--- app/src/main/java/ru/cj264/geekbrains/movie_finder/MainActivity.kt
+++ app/src/main/java/ru/cj264/geekbrains/movie_finder/MainActivity.kt
@@ -1,11 +1,17 @@
 package ru.cj264.geekbrains.movie_finder
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.button).setOnClickListener {
+            Toast.makeText(this, "Button pressed", Toast.LENGTH_LONG).show()
+        }
     }
 }
