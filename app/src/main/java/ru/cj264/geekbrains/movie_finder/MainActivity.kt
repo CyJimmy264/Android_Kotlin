bce2124aaa6d18b41995b8fd58f1da11ff402abe
mveynberg <mv@cj264.ru>
2021-04-30 16:54:11 +0500

    Step 3. Convert MainActivity from Java to Kotlin

---
+++ app/src/main/java/ru/cj264/geekbrains/movie_finder/MainActivity.kt
@@ -0,0 +1,11 @@
+package ru.cj264.geekbrains.movie_finder
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
