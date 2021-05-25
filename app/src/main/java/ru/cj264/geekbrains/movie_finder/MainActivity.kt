0dc00a40e4b9731c66e2fc2d5794faa5e5fcb5e4
mveynberg <mv@cj264.ru>
2021-05-25 16:06:05 +0500

    Create new Kotlin Fragment + ViewModel project

--- app/src/main/java/ru/cj264/geekbrains/movie_finder/MainActivity.kt
+++ app/src/main/java/ru/cj264/geekbrains/movie_finder/MainActivity.kt
@@ -1,11 +1,18 @@
 package ru.cj264.geekbrains.movie_finder
 
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ru.cj264.geekbrains.movie_finder.ui.main.MainFragment
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
     }
 }
